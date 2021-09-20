6b7460971d12bcec66baa42edc02e097b003eb58
Jakub Belescak <jakub.belescak@centrum.cz>
2021-09-20 18:24:09 +0200

    the templates were missing in the pip

--- setup.py
+++ setup.py
@@ -5,14 +5,16 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="mlmprovisions", # Replace with your own username
-    version="0.1.5",
+    version="0.1.6",
     author="Belda",
     author_email="jakub.belescak@centrum.cz",
     description="Reusable Django app to track provisions for affiliate partners. Includes tools for multi-level marketing provisions.",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/belda/mlmprovisions",
-    packages=setuptools.find_packages(),
+    packages=['mlmtools'],
+    include_package_data=True,
+    zip_safe=False,
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
